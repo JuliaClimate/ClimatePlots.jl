c05050113394b9d23dbb82be354f55e5cdede41c
Philippe Roy <borghor@yahoo.ca>
2020-01-09 21:04:39 -0500

    updated main file and Project.toml

---
+++ src/ClimatePlots.jl
@@ -0,0 +1,25 @@
+module ClimatePlots
+
+
+using Reexport
+@reexport ClimateTools
+using PyCall
+using PyPlot
+
+const basemap = PyNULL()
+const mpl = PyNULL()
+const cmocean = PyNULL()
+# const scipy = PyNULL()
+
+function __init__()
+    copy!(mpl, pyimport_conda("matplotlib", "matplotlib"))
+    copy!(basemap, pyimport_conda("mpl_toolkits.basemap", "basemap"))
+    copy!(cmocean, pyimport_conda("cmocean", "cmocean", "conda-forge"))
+  # copy!(scipy, pyimport_conda("scipy.interpolate", "scipy"))
+end
+
+include("mapping.jl")
+
+export plot, mapclimgrid, hist
+
+end # module
