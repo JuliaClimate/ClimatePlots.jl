aea204eefae5acb5159c3180bfef15ef9911a05b
Philippe Roy <borghor@yahoo.ca>
2020-01-17 09:24:54 -0500

    Specified mapclimgrid from ClimatePlots

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,12 +12,12 @@ filenc = joinpath(dirname(@__FILE__), "data", "sresa1b_ncar_ccsm3-example.nc")
 
 C = load(filenc, "tas")
 # msk = inpolygrid(C.longrid, C.latgrid, P)
-status, figh = mapclimgrid(C); @test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C); @test status == true; PyPlot.close()
 
 regions = ["World", "WorldAz", "Canada", "Quebec", "Quebec_agricole", "South_Quebec", "Americas", "NorthAmerica", "SouthAmerica", "Greenwich", "Outaouais", "Laurentides", "Estrie", "Arctic", "Antarctic", "Africa", "Europe", "Asia", "West-Asia"]
 
 for iregion in regions
-    status, figh = mapclimgrid(C, region=iregion);@test status == true; PyPlot.close()
+    status, figh = ClimatePlots.mapclimgrid(C, region=iregion);@test status == true; PyPlot.close()
 end
 
 # precip
@@ -26,24 +26,24 @@ C = load(filenc, "pr", data_units="mm") + 2.0
 regions = ["World", "WorldAz", "Canada", "Quebec", "Quebec_agricole", "South_Quebec", "Americas", "NorthAmerica", "SouthAmerica", "Greenwich", "Outaouais", "Laurentides", "Estrie", "Arctic", "Antarctic", "Africa", "Europe", "Asia", "West-Asia"]
 
 for iregion in regions
-    status, figh = mapclimgrid(C, region=iregion);@test status == true; PyPlot.close()
+    status, figh = ClimatePlots.mapclimgrid(C, region=iregion);@test status == true; PyPlot.close()
 end
 
-status, figh = mapclimgrid(C, start_date=(2000, 05, 16, 12), end_date=(2000, 05, 16, 12));@test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C, start_date=(2000, 05, 16, 12), end_date=(2000, 05, 16, 12));@test status == true; PyPlot.close()
 
 
 # ua wind
 filenc = joinpath(dirname(@__FILE__), "data", "sresa1b_ncar_ccsm3-example.nc")
 
 C = load(filenc, "ua")
-status, figh = mapclimgrid(C, level = 3);@test status == true; PyPlot.close() # given level
-status, figh = mapclimgrid(C);@test status == true; PyPlot.close() # feeding a 4D field
+status, figh = ClimatePlots.mapclimgrid(C, level = 3);@test status == true; PyPlot.close() # given level
+status, figh = ClimatePlots.mapclimgrid(C);@test status == true; PyPlot.close() # feeding a 4D field
 
 # Empty maps
 regions = ["World", "WorldAz", "Canada", "Quebec", "Quebec_agricole", "South_Quebec", "Americas", "NorthAmerica", "SouthAmerica", "Greenwich", "Outaouais", "Laurentides", "Estrie", "Arctic", "Antarctic", "Africa", "Europe", "Asia", "West-Asia"]
 
 for iregion in regions
-    status, figh = mapclimgrid(region=iregion);@test status == true; PyPlot.close()
+    status, figh = ClimatePlots.mapclimgrid(region=iregion);@test status == true; PyPlot.close()
 end
 
 # DUMMY MAPS
@@ -57,9 +57,9 @@ varattribs = Dict(["standard_name" => "random noise"])
 axisdata = AxisArray(data, Axis{:lon}(lon), Axis{:lat}(lat), Axis{:time}(timeV))
 C = ClimateTools.ClimGrid(axisdata, variable = "psl", typeofvar="psl", longrid=longrid, latgrid=latgrid, dimension_dict=dimension_dict, varattribs=varattribs)
 
-status, figh = mapclimgrid(C);@test status == true; PyPlot.close()
-status, figh = mapclimgrid(C, center_cs=true, caxis=[200, 250]);@test status == true; PyPlot.close()
-status, figh = mapclimgrid(C, center_cs=true, surface=:pcolormesh);@test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C);@test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C, center_cs=true, caxis=[200, 250]);@test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C, center_cs=true, surface=:pcolormesh);@test status == true; PyPlot.close()
 
 
 lon = collect(-180.0:180.0)
@@ -72,7 +72,7 @@ varattribs = Dict(["standard_name" => "random noise"])
 axisdata = AxisArray(data, Axis{:lon}(lon), Axis{:lat}(lat), Axis{:time}(timeV))
 C = ClimateTools.ClimGrid(axisdata, variable = "psl", typeofvar="psl", longrid=longrid, latgrid=latgrid, dimension_dict=dimension_dict, varattribs=varattribs)
 
-status, figh = mapclimgrid(C);@test status == true; PyPlot.close()
+status, figh = ClimatePlots.mapclimgrid(C);@test status == true; PyPlot.close()
 
 lon = collect(-180.0:180.0)
 lat = collect(-90.0:90.0)
