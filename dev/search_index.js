ab966742c593f082595e9bc5c19648ff385aee1b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-02-08 14:47:19 +0000

    build based on 28487d9

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"-","title":"-","text":"ClimatePlots is the plotting library that supports ClimateTools.jl.","category":"page"},{"location":"#","page":"-","title":"-","text":"Documentation for both ClimatePlots and ClimateTools can be found here and here","category":"page"},{"location":"#","page":"-","title":"-","text":"(Image: ) (Image: )","category":"page"}]
+}
